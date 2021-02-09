908172d5cea2c360e57c6498c489bf8e8b880bda
RupenderT <35565641+RupenderT@users.noreply.github.com>
2021-02-09 14:57:59 +0530

    Update where cluase

--- src/WhereClause.ts
+++ src/WhereClause.ts
@@ -5,49 +5,49 @@ export class WhereClause<T> {
 
     above(val: string):T {
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: val, operator: ">" });
+        obj.strWhere.push({ name: this.on.toString(), value: val.toString(), operator: ">" });
         return obj;
     }
     aboveOrEqual(val:string):T {
 
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: val, operator: ">=" });
+        obj.strWhere.push({ name: this.on.toString(), value: val.toString(), operator: ">=" });
         return obj;
     }
     below(val:string):T {
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: val, operator: "<" });
+        obj.strWhere.push({ name: this.on.toString(), value: val.toString(), operator: "<" });
         return obj;
     }
     belowOrEqual(val:string):T {
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: val, operator: "<=" });
+        obj.strWhere.push({ name: this.on.toString(), value: val.toString(), operator: "<=" });
         return obj;
     }
     between(val1:string, val2:string):T {
         var obj = this.getCopy();
-        obj.betweenArr.push({ name: this.on, value1: val1, value2: val2 });
+        obj.betweenArr.push({ name: this.on.toString(), value1: val1.toString(), value2: val2.toString() });
         return obj;
     }
     equals(val:string):T {
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: val, operator: "=" });
+        obj.strWhere.push({ name: this.on.toString(), value: val.toString(), operator: "=" });
         return obj;
     }
     notEquals(val:string):T{
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: val, operator: "<>" });
+        obj.strWhere.push({ name: this.on.toString(), value: val.toString(), operator: "<>" });
         return obj;
     }
 
     isNull():T {
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: "AUTOSQLNULL", operator: "IS" });
+        obj.strWhere.push({ name: this.on.toString(), value: "AUTOSQLNULL", operator: "IS" });
         return obj;
     }
     isNotNull():T {
         var obj = this.getCopy();
-        obj.strWhere.push({ name: this.on, value: "AUTOSQLNULL", operator: "IS NOT" });
+        obj.strWhere.push({ name: this.on.toString(), value: "AUTOSQLNULL", operator: "IS NOT" });
         return obj;
     }
 
